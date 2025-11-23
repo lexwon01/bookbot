f777761bbb9ab8e1810cbfae31e8582554bc8f52
lexwon <lexwon01@gmail.com>
2025-11-23 15:12:14 +0000

    CH3:L1

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from stats import get_num_words
+from stats import get_num_chars
+from stats import chars_dict_to_sorted_list
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -11,8 +13,22 @@ def main():
     text = get_book_text(book_path)
 
     num_words = get_num_words(text)
+    num_chars = get_num_chars(text)
+    chars_sorted_list = chars_dict_to_sorted_list(num_chars)
 
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -2,3 +2,30 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
+def get_num_chars(text):
+    lower_words = text.lower()
+    
+    num_char = {}
+
+    for char in lower_words:
+        if char in num_char:
+            num_char[char] += 1
+        else:
+            num_char[char] = 1
+    return num_char
+
+def sort_on(items):
+    return items["num"]
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    
+    for ch in num_chars_dict:
+        tmp_dict = {"char": ch, "num": num_chars_dict[ch]}
+        
+        sorted_list.append(tmp_dict)
+    
+    sorted_list.sort(reverse=True, key=sort_on)
+    
+    return sorted_list
+

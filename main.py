3114037c076932304161968d74e37b2417e83847
lexwon <lexwon01@gmail.com>
2025-11-24 07:24:11 +0000

    added some more functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import get_num_words
 from stats import get_num_chars
 from stats import chars_dict_to_sorted_list
+import sys
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -9,9 +10,13 @@ def get_book_text(path_to_file):
 
 def main():
 
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
+    book_path = sys.argv[1]
+
+    text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_chars = get_num_chars(text)
     chars_sorted_list = chars_dict_to_sorted_list(num_chars)
